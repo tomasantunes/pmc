3c14b21e5f1d5b7e2804df00eac332e1814241c0
Tomás Antunes <tomasantunes@gmail.com>
2023-03-31 14:58:19 +0100

    Increase strikethrough thickness and add folder title

--- frontend/src/components/TasksPage.js
+++ frontend/src/components/TasksPage.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Tasks from './Tasks';
 import $ from 'jquery';
+import axios from 'axios';
+import config from '../config';
 
 window.jQuery = $;
 window.$ = $;
@@ -8,10 +10,33 @@ global.jQuery = $;
 window.bootstrap = require('bootstrap');
 
 export default function TasksPage({folder_id}) {
+  const [folder, setFolder] = useState({});
+
+  function getFolderInfo(folder_id) {
+    axios.get(config.BASE_URL + "/api/get-folder", {
+      params: {
+        folder_id: folder_id
+      }
+    })
+    .then(function(response) {
+      if (response.data.status === "OK") {
+        setFolder(response.data.data);
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+      alert("Error: " + err.message);
+    });
+
+  }
+
+  useEffect(() => {
+    getFolderInfo(folder_id);
+  }, []);
   return (
     <div className="page">
         <div style={{textAlign: "center"}}>
-            <h3>Tasks</h3>
+            <h3>{folder.name}</h3>
         </div>
         <Tasks folder_id={folder_id} />
     </div>
