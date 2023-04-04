d4b53f1f74652cb3586cab99224b37945667b622
Tomás Antunes <tomasantunes@gmail.com>
2023-04-04 18:41:49 +0100

    Implement login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './components/Home';
 import FolderPage from './components/FolderPage';
+import Login from './components/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/folder/:id" element={<FolderPage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
