ea7ded0588bdc62b64e7dcc87d3fe331e1160273
Tomás Antunes <tomasantunes@gmail.com>
2023-09-23 11:22:46 +0100

    Generate motivational text.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Sidebar from './Sidebar';
 import Stats from './Stats';
 import $ from 'jquery';
+import axios from 'axios';
+import config from '../config.json';
 
 window.jQuery = $;
 window.$ = $;
@@ -9,10 +11,28 @@ global.jQuery = $;
 window.bootstrap = require('bootstrap');
 
 export default function Home() {
+  const [motivationalText, setMotivationalText] = useState('');
+
+  function getMotivationalText() {
+    axios.get(config.BASE_URL + '/api/generate-motivational-text')
+      .then(res => {
+        setMotivationalText(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    getMotivationalText();
+  });
+
   return (
     <>
       <Sidebar />
       <div className="page">
+        <h3>Motivation</h3>
+        <p>{motivationalText}</p>
         <Stats />
       </div>
     </>
