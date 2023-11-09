bc65f88b79d95cfbca9c88354035fb840213d143
Tomás Antunes <tomasantunes@gmail.com>
2023-11-09 10:58:52 +0000

    Add calendar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -104,6 +104,7 @@ export default function Sidebar() {
           <h1><b>PMC</b></h1>
           <ul className="menu">
               <li><Link to="/home">Home</Link></li>
+              <li><Link to="/calendar">Calendar</Link></li>
               <li><Link to="/github-tasks">Github Tasks</Link></li>
               <li><Link to="/motivation">Motivation</Link></li>
               <li><a href="#" onClick={openAddFolder}>Add Folder</a></li>
