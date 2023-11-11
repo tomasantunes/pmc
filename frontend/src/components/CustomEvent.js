715906a4a0ae05a828aa4f2f851d5be1b09ffbeb
Tomás Antunes <tomasantunes@gmail.com>
2023-11-11 16:40:55 +0000

    Implement margins on events based on time

--- frontend/src/components/CustomEvent.js
+++ frontend/src/components/CustomEvent.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const customEvent = (props) => {
     console.log(props);
+    var start_m = Number(props.start.format("m"));
+    var end_m = Number(props.end.format("m"));
+    var marginTop = 50 * start_m / 60;
+    var marginBottom = 50 * (60 - end_m) / 60;
+    
     return (
-        <div className="customEvent">
+        <div className="customEvent" style={{marginTop: marginTop, height: "calc(100% - " + marginBottom + "px)"}}>
             <div className="small-text">{props.value}</div>
         </div>
     );
