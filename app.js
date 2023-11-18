544639885c8941a950b25cf58024da412f1f34b5
Tomás Antunes <tomasantunes@gmail.com>
2023-11-18 16:38:33 +0000

    Allow the user to add recurrent tasks without setting the time.

--- app.js
+++ app.js
@@ -324,16 +324,25 @@ app.post("/api/add-recurrent-task", (req, res) => {
   var days = req.body.days;
   var sort_index = req.body.sort_index;
 
-  var dt1 = new Date();
-  var t1 = start_time.split(":");
-  dt1.setHours(t1[0], t1[1], 0);
-  var start_time2 = dt1.toISOString().slice(0, 19).replace('T', ' ');
-  var dt2 = new Date();
-  var t2 = end_time.split(":");
-  dt2.setHours(t2[0], t2[1], 0);
-  var end_time2 = dt2.toISOString().slice(0, 19).replace('T', ' ');
-
-
+  var start_time2;
+  var end_time2;
+  var has_time = false;
+  if (typeof start_time == "undefined" || start_time == "" || typeof end_time == "undefined" || end_time == "") {
+    start_time2 = new Date('1970-01-01Z00:00:00:000').toISOString().slice(0, 19).replace('T', ' ');
+    end_time2 = new Date('1970-01-01Z00:00:00:000').toISOString().slice(0, 19).replace('T', ' ');
+  }
+  else {
+    var dt1 = new Date();
+    var t1 = start_time.split(":");
+    dt1.setHours(t1[0], t1[1], 0);
+    start_time2 = dt1.toISOString().slice(0, 19).replace('T', ' ');
+    var dt2 = new Date();
+    var t2 = end_time.split(":");
+    dt2.setHours(t2[0], t2[1], 0);
+    end_time2 = dt2.toISOString().slice(0, 19).replace('T', ' ');
+    has_time = true;
+  }
+  
   var sql = "INSERT INTO tasks (folder_id, description, start_time, end_time, type, days, sort_index, is_done) VALUES (?, ?, ?, ?, ?, ?, ?, 0)";
   con.query(sql, [folder_id, description, start_time2, end_time2, "recurrent", days, sort_index], async function (err, result) {
     if (err) {
@@ -349,18 +358,20 @@ app.post("/api/add-recurrent-task", (req, res) => {
       var sql2 = "INSERT INTO recurrent_checks (task_id, date, is_done) VALUES (?, ?, 0)";
       await con2.query(sql2, [result.insertId, dates[i].toISOString().slice(0, 10)]);
 
-      var start_date = dates[i];
-      var st = start_time.split(":");
-      start_date.setHours(st[0], st[1], 0);
-      start_date = start_date.toISOString().slice(0, 19).replace('T', ' ');
-      var end_date = dates[i];
-      var et = end_time.split(":");
-      end_date.setHours(et[0], et[1], 0);
-      end_date = end_date.toISOString().slice(0, 19).replace('T', ' ');
-      console.log(start_date);
-      console.log(end_date);
-      var sql3 = "INSERT INTO events (task_id, start_date, end_date, description) VALUES (?, ?, ?, ?)";
-      await con2.query(sql3, [result.insertId, start_date, end_date, description]);
+      if (has_time) {
+        var start_date = dates[i];
+        var st = start_time.split(":");
+        start_date.setHours(st[0], st[1], 0);
+        start_date = start_date.toISOString().slice(0, 19).replace('T', ' ');
+        var end_date = dates[i];
+        var et = end_time.split(":");
+        end_date.setHours(et[0], et[1], 0);
+        end_date = end_date.toISOString().slice(0, 19).replace('T', ' ');
+        console.log(start_date);
+        console.log(end_date);
+        var sql3 = "INSERT INTO events (task_id, start_date, end_date, description) VALUES (?, ?, ?, ?)";
+        await con2.query(sql3, [result.insertId, start_date, end_date, description]);
+      }
     }
     res.json({status: "OK", data: "Task has been added successfully."});
   });
@@ -702,8 +713,27 @@ app.post("/api/edit-recurrent-task", (req, res) => {
   var end_time = req.body.end_time;
   var days = req.body.days;
 
+  var start_time2;
+  var end_time2;
+  var has_time = false;
+  if (typeof start_time == "undefined" || start_time == "" || typeof end_time == "undefined" || end_time == "") {
+    start_time2 = new Date('1970-01-01Z00:00:00:000').toISOString().slice(0, 19).replace('T', ' ');
+    end_time2 = new Date('1970-01-01Z00:00:00:000').toISOString().slice(0, 19).replace('T', ' ');
+  }
+  else {
+    var dt1 = new Date();
+    var t1 = start_time.split(":");
+    dt1.setHours(t1[0], t1[1], 0);
+    start_time2 = dt1.toISOString().slice(0, 19).replace('T', ' ');
+    var dt2 = new Date();
+    var t2 = end_time.split(":");
+    dt2.setHours(t2[0], t2[1], 0);
+    end_time2 = dt2.toISOString().slice(0, 19).replace('T', ' ');
+    has_time = true;
+  }
+
   var sql = "UPDATE tasks SET description = ?, start_time = ?, end_time = ?, days = ? WHERE id = ?";
-  con.query(sql, [description, start_time, end_time, days, task_id], async function (err, result) {
+  con.query(sql, [description, start_time2, end_time2, days, task_id], async function (err, result) {
     if (err) {
       console.log(err);
       res.json({status: "NOK", error: err.message});
@@ -722,18 +752,20 @@ app.post("/api/edit-recurrent-task", (req, res) => {
       var sql2 = "INSERT INTO recurrent_checks (task_id, date, is_done) VALUES (?, ?, 0)";
       await con2.query(sql2, [task_id, dates[i].toISOString().slice(0, 10)]);
 
-      var start_date = dates[i];
-      var st = start_time.split(":");
-      start_date.setHours(st[0], st[1], 0);
-      start_date = start_date.toISOString().slice(0, 19).replace('T', ' ');
-      var end_date = dates[i];
-      var et = end_time.split(":");
-      end_date.setHours(et[0], et[1], 0);
-      end_date = end_date.toISOString().slice(0, 19).replace('T', ' ');
-      console.log(start_date);
-      console.log(end_date);
-      var sql3 = "INSERT INTO events (task_id, start_date, end_date, description) VALUES (?, ?, ?, ?)";
-      await con2.query(sql3, [task_id, start_date, end_date, description]);
+      if (has_time) {
+        var start_date = dates[i];
+        var st = start_time.split(":");
+        start_date.setHours(st[0], st[1], 0);
+        start_date = start_date.toISOString().slice(0, 19).replace('T', ' ');
+        var end_date = dates[i];
+        var et = end_time.split(":");
+        end_date.setHours(et[0], et[1], 0);
+        end_date = end_date.toISOString().slice(0, 19).replace('T', ' ');
+        console.log(start_date);
+        console.log(end_date);
+        var sql3 = "INSERT INTO events (task_id, start_date, end_date, description) VALUES (?, ?, ?, ?)";
+        await con2.query(sql3, [task_id, start_date, end_date, description]);
+      }
     }
 
     res.json({status: "OK", data: "Task has been updated successfully."});
@@ -818,7 +850,15 @@ app.post("/api/delete-task", (req, res) => {
         console.log(err2);
         res.json({status: "NOK", error: err2.message});
       }
-      res.json({status: "OK", data: "Task has been deleted successfully."});
+
+      var sql3 = "DELETE FROM events WHERE task_id = ?";
+      con.query(sql3, [task_id], function (err3, result3) {
+        if (err3) {
+          console.log(err3);
+          res.json({status: "NOK", error: err3.message});
+        }
+        res.json({status: "OK", data: "Task has been deleted successfully."});
+      });
     });
   });
 });
