8dc0a560e189a3c7fac196bd59137843a3e927f0
Tomás Antunes <tomasantunes@gmail.com>
2025-09-19 14:34:56 +0100

    Fix error on Bootstrap JS and add task on TODO.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import * as bootstrap from 'bootstrap'
 import './index.css'
 import App from './App.jsx'
+
+window.bootstrap = bootstrap
 
 createRoot(document.getElementById('root')).render(
     <App />,
