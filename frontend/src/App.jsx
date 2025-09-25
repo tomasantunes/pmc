2e1088801734d5a3556d3795ddb9e91883e42df5
Tomás Antunes <tomasantunes@gmail.com>
2025-09-25 10:31:46 +0100

    Redirect index to home on App.jsx and add catch-all route on back-end.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Home from './components/Home';
 import FolderPage from './components/FolderPage';
 import Login from './components/Login';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import GithubTasks from './components/GithubTasks';
 import Motivation from './components/Motivation';
 import Calendar from './components/Calendar';
@@ -13,6 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
         <Route path="/folder/:id" element={<FolderPage />} />
         <Route path="/login" element={<Login />} />
