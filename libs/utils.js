b01835d9eaa454a3be1143078a453ff387b41c23
Tomás Antunes <tomasantunes@gmail.com>
2025-10-02 16:13:55 +0100

    Set default value for new tasks to be empty. Fix error where datetimes are set to one hour before. Fix error where new tasks don't go the end of the list.

--- libs/utils.js
+++ libs/utils.js
@@ -49,17 +49,35 @@ function previousMonday() {
   return d;
 }
 
+function toLocaleISOString(date) {
+    function pad(number) {
+        if (number < 10) {
+            return '0' + number;
+        }
+        return number;
+    }
+
+    return date.getFullYear() +
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        'T' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes()) +
+        ':' + pad(date.getSeconds()) ;
+}
+
 module.exports = {
     addDays,
     getRangeOfDates,
     getDatesUntilNextYear,
     nextDate,
     previousMonday,
+    toLocaleISOString,
     default: {
         addDays,
         getRangeOfDates,
         getDatesUntilNextYear,
         nextDate,
-        previousMonday
+        previousMonday,
+        toLocaleISOString
     }
 };
