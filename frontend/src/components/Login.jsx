754328d01d212f2298ff3b2c3eae7fd32508dbd4
Tomás Antunes <tomasantunes@gmail.com>
2025-10-05 16:19:52 +0100

    Make Login Form compatible the browser's password manager.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -20,11 +20,18 @@ export default function Login() {
     setPass(event.target.value);
   }
 
-  function requestLogin() {
+  function requestLogin(e) {
+    e.preventDefault();
     axios.post(config.BASE_URL + "/api/check-login", {user, pass})
-    .then(res => {
+    .then(async res => {
       if (res.data.status == "OK") {
-        console.log(res.data.data);
+        if ("credentials" in navigator) {
+          const cred = new PasswordCredential({
+            id: user,
+            password: pass,
+          });
+          await navigator.credentials.store(cred);
+        }
         navigate("/home");
       }
       else {
@@ -38,26 +45,51 @@ export default function Login() {
 
   const handleKeyDown = e => {
     if (e.keyCode === 13) {
-      requestLogin();
+      e.preventDefault();
+      e.target.form.dispatchEvent(new Event("submit", {cancelable: true, bubbles: true}));
     }
   };
 
   return (
-    <div className="login-box">
-      <div style={{textAlign: "center"}}>
-        <h3>Login</h3>
-      </div>
-      <div className="form-group">
-          <label>Username</label>
-          <input type="text" className="form-control" value={user} onChange={changeUser} onKeyDown={handleKeyDown} />
-      </div>
-      <div className="form-group mb-4">
-          <label>Password</label>
-          <input type="password" className="form-control" value={pass} onChange={changePass} onKeyDown={handleKeyDown} />
-      </div>
-      <div style={{textAlign: "right"}}>
-          <button className="btn btn-primary" onClick={requestLogin}>Login</button>
-      </div>
-    </div>
+    <>
+      <form onSubmit={(e) => requestLogin(e)} method="POST" action="/login" autoComplete="on" className="login-box">
+        <div style={{textAlign: "center"}}>
+          <h3>Login</h3>
+        </div>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            autoComplete="username"
+            value={user}
+            onChange={changeUser}
+            onKeyDown={handleKeyDown}
+            className="form-control"
+            required
+          />
+        </div>
+
+        <div className="form-group mb-4">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="current-password"
+            value={pass}
+            onChange={changePass}
+            onKeyDown={handleKeyDown}
+            className="form-control"
+            required
+          />
+        </div>
+
+        <div className="text-end">
+            <button className="btn btn-primary" type="submit">Login</button>
+        </div>
+      </form>
+    </>
   )
 }
