8c23b5f604e3f04d83804380ac9e8de24ac1add0
Tomás Antunes <tomasantunes@gmail.com>
2025-10-09 08:18:06 +0100

    Select timeslot to create event on calendar.

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,20 +1,40 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import Sidebar from './Sidebar';
 import Stats from './Stats';
 import axios from 'axios';
 import config from '../config.json';
 import moment from 'moment';
 import FullCalendar from '@fullcalendar/react';
-import timeGridWeek from '@fullcalendar/timegrid'
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 export default function Home() {
+  const calendarRef = useRef(null);
+
+
+  const handleSelect = (info) => {
+    const calendarApi = calendarRef.current.getApi();
+
+    const title = prompt('New event title:');
+    if (title) {
+      calendarApi.addEvent({
+        title,
+        start: info.start,
+        end: info.end,
+      });
+    }
+  };
+
   return (
     <>
       <Sidebar />
       <div className="page">
         <FullCalendar
-          plugins={[ timeGridWeek ]}
+          ref={calendarRef}
+          plugins={[timeGridPlugin, interactionPlugin]}
           initialView="timeGridWeek"
+          selectable={true}
+          select={handleSelect}
         />
       </div>
     </>
