be8c0f69ea98b8e9811c1ad18a49d910b32d1c9d
Tomás Antunes <tomasantunes@gmail.com>
2025-10-18 11:55:45 +0100

    Create SimpleToDoTable and MySQL table for Daily To-Dos.

--- frontend/src/components/SimpleToDoTable.jsx
+++ frontend/src/components/SimpleToDoTable.jsx
@@ -9,7 +9,31 @@ export default function DailyToDos({title, tasks, setTasks}) {
   return (
     <>
       <h2>{title}</h2>
-      <table></table>
+      <table className="table table-striped table-bordered">
+        <thead className="table-dark">
+          <tr>
+            <th></th>
+            <th>Task</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((t) => {
+            <tr key={t.id}>
+              <td><input type="checkbox" /></td>
+              <td>{t.description}</td>
+              <td></td>
+            </tr>
+          })}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td></td>
+            <td><input type="text" className="form-control" value={newTaskDescription} onChange={changeNewTaskDescription} /></td>
+            <td><button className="btn btn-success" onClick={addTask}><i className="fas fa-plus" /></button></td>
+          </tr>
+        </tfoot>
+      </table>
     </>
   )
 }
