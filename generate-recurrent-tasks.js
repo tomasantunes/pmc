457b5b176d925bc331407267cd9cd66f361dd2f7
Tomás Antunes <tomasantunes@gmail.com>
2025-11-06 10:16:11 +0000

    Generate events for recurrent tasks every year.

--- generate-recurrent-tasks.js
+++ generate-recurrent-tasks.js
@@ -16,6 +16,21 @@ async function generateRecurrentTasks() {
                 var sql2 = "INSERT INTO recurrent_checks (task_id, date, is_done) VALUES (?, ?, ?)";
                 await con2.query(sql2, [task.id, dates[i].toISOString().slice(0, 10), 0]);
             }
+
+            if (task.start_time != "1970-01-01 00:00:00" && task.end_time != "1970-01-01 00:00:00") {
+                for (var i in dates) {
+                    var start_date = dates[i];
+                    var st = task.start_time.split(" ")[1].split(":");
+                    start_date.setHours(st[0], st[1], 0);
+                    start_date = start_date.toISOString().slice(0, 19).replace('T', ' ');
+                    var end_date = dates[i];
+                    var et = task.end_time.split(" ")[1].split(":");
+                    end_date.setHours(et[0], et[1], 0);
+                    end_date = end_date.toISOString().slice(0, 19).replace('T', ' ');
+                    var sql3 = "INSERT INTO events (task_id, start_date, end_date, description) VALUES (?, ?, ?, ?)";
+                    await con2.query(sql3, [task.id, start_date, end_date, task.description]);
+                }
+            }
         }
         console.log("Recurrent tasks generated.");
     });
