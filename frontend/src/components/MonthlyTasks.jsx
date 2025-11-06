c14862fc8d169e3c34b43259e749d1d7847d74e4
Tomás Antunes <tomasantunes@gmail.com>
2025-11-06 09:04:46 +0000

    Fix error in monthly tasks.

--- frontend/src/components/MonthlyTasks.jsx
+++ frontend/src/components/MonthlyTasks.jsx
@@ -237,7 +237,8 @@ export default function MonthlyTasks({ folder_id, folder }) {
       checks_visible = checks_visible.map(Number);
       var idx_arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
       for (var i in dates) {
-        if (dateIsLessThan(dates[i], new Date(task.created_at.split("T")[0]))) {
+        var lastDayofMonth = new Date(dates[i].getFullYear(), dates[i].getMonth() + 1, 0);
+        if (dateIsLessThan(lastDayofMonth, new Date(task.created_at.split("T")[0]))) {
           checks_visible = checks_visible.filter((item) => {
             return idx_arr.indexOf(item) != Number(i);
           });
